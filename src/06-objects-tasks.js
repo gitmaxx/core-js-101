72f32d8f52ea5a29c11c4a118a68dd4c3085d352
Akulinichev Maxim <mxmrus@gmail.com>
2019-12-20 00:08:45 +0300

    refactor: linting

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -23,7 +23,7 @@
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
+  this.getArea = function t() {
     return this.width * this.height;
   };
 }
