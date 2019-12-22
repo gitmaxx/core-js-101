da31a484380870170c3bfbda4f64f93e4c3d6c22
Akulinichev Maxim <mxmrus@gmail.com>
2019-12-22 18:32:36 +0300

    feat: add last solution

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -124,34 +124,102 @@ function fromJSON(proto, json) {
  *  For more examples see unit tests.
  */
 
+
+function Selector(str, type) {
+  this.types = [type];
+  switch (type) {
+    case 'class':
+      this.str = `.${str}`;
+      break;
+    case 'id':
+      this.str = `#${str}`;
+      break;
+    case 'attr':
+      this.str = `[${str}]`;
+      break;
+    case 'pseudoClass':
+      this.str = `:${str}`;
+      break;
+    case 'pseudoElement':
+      this.str = `::${str}`;
+      break;
+    default: this.str = str;
+  }
+  this.stringify = function stringify() {
+    return this.str;
+  };
+  this.id = function id(value) {
+    if (this.types.includes('id')) { throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector'); }
+    if (this.types[this.types.length - 1] !== 'element') { throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'); }
+    this.types.push('id');
+    this.str = `${this.str}#${value}`;
+    return this;
+  };
+  this.class = function cl(value) {
+    if (this.types[this.types.length - 1] !== 'id' && this.types[this.types.length - 1] !== 'element' && this.types[this.types.length - 1] !== 'class') { throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'); }
+    this.str = `${this.str}.${value}`;
+    return this;
+  };
+  this.element = function el(value) {
+    if (this.types.includes('element')) { throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector'); }
+    if (this.types[this.types.length - 1] !== 'element') { throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'); }
+    this.types.push('id');
+    this.types.push('element');
+    this.str = `${this.str}${value}`;
+    return this;
+  };
+  this.attr = function attr(value) {
+    if (this.types[this.types.length - 1] === 'pseudoClass' || this.types[this.types.length - 1] === 'pseudoElement') { throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'); }
+    this.str = `${this.str}[${value}]`;
+    return this;
+  };
+  this.pseudoClass = function pseudoClass(value) {
+    if (this.types[this.types.length - 1] === 'pseudoElement') { throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'); }
+    this.str = `${this.str}:${value}`;
+    return this;
+  };
+  this.pseudoElement = function pseudoElement(value) {
+    if (this.types.includes('pseudoElement')) { throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector'); }
+    this.types.push('pseudoElement');
+    this.str = `${this.str}::${value}`;
+    return this;
+  };
+}
+
+function ComboSelector(s1, s2, combo) {
+  this.str = `${s1.str} ${combo} ${s2.str}`;
+  this.stringify = function stringify() {
+    return this.str;
+  };
+}
+
 const cssSelectorBuilder = {
-  /* eslint-disable */
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return new Selector(value, 'element');
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new Selector(value, 'id');
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Selector(value, 'class');
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Selector(value, 'attr');
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Selector(value, 'pseudoClass');
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Selector(value, 'pseudoElement');
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return new ComboSelector(selector1, selector2, combinator);
   },
 };
 
